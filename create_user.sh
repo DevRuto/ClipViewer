435316881f1ba59984a396b6d5ced5ba209e3b52
Ruto <devruto@gmail.com>
2026-01-14 22:48:49 -0500

    Helper shell scripts for create user and api key

---
+++ create_user.sh
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env bash
+docker compose exec -T db psql -U "clipviewer" -d "clipviewer" \
+-v username="$1" <<'SQL'
+INSERT INTO "Users" ("Username", "ApiKey", "CreatedAt")
+VALUES (:'username', gen_random_uuid(), NOW())
+RETURNING *;
+SQL
---
+++ update_user.sh
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env bash
+docker compose exec -T db psql -U "clipviewer" -d "clipviewer" \
+-v username="$1" <<'SQL'
+UPDATE "Users"
+SET "ApiKey" = gen_random_uuid()
+WHERE "Username" = :'username'
+RETURNING *;
+SQL
